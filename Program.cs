d7884fd0f9e3aeb8659f7feb16a1b6f52b49ff33
Nikolai <agro034@mail.ru>
2023-03-06 13:39:21 +0300

    Задача 56

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int numRows = SetNumber("Rows ");
+int numColumns = SetNumber("Columns ");
+
+int numMaxValue = SetNumber("Max number ");
+int numMinValue = SetNumber("Min number ");
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+System.Console.WriteLine();
+minString(matrix);
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void minString(int[,] matrix)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        // Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {index+1}. ");
+}
